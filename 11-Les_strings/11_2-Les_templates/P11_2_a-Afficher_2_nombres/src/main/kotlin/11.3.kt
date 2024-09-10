e16f6d22a31c4ccab1e9333bd51be5ae6da49c93
Ekoli Hugo <ekoli.hugo@gmail.com>
2024-09-10 08:58:18 +0200

    Ajout des exo 11

--- 11-Les_strings/11_2-Les_templates/P11_2_a-Afficher_2_nombres/src/main/kotlin/11.3.kt
+++ 11-Les_strings/11_2-Les_templates/P11_2_a-Afficher_2_nombres/src/main/kotlin/11.3.kt
@@ -1,5 +1,5 @@
 fun main() {
     var a = readln().toInt()
     var b = readln().toInt()
-    println("$a $b")
+    println("$a plus $b equals ${a+b}")
 }
---
+++ 11-Les_strings/11_2-Les_templates/P11_2_a-Afficher_2_nombres/src/main/kotlin/11.3a.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var a = readln()
+    var b = readln()
+    var c = readln()
+    println(a+b==c)
+}
